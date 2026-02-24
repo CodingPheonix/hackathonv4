3e24a3ffff8bac0fcfb5077b006b692f94650896
Priyanshu-Biswas <priyanshubiswas16082004@gmail.com>
2026-02-24 16:32:51 +0530

    final design of landing page

--- frontend/src/Component/Header.jsx
+++ frontend/src/Component/Header.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Header = () => {
   return (
 
-    <header classNameName="flex justify-between items-center px-8 py-5">
+    <header className="flex justify-between items-center px-8 py-5">
       <div className="flex items-center space-x-2">
         <div className="bg-green-500 p-2 rounded-md">
           <span className="text-white text-lg font-bold">🚜</span>
         </div>
-        <h1 className="text-xl font-semibold text-gray-800">AgriSmart.AI</h1>
+        <h1 className="text-xl font-bold text-gray-800">AgriSmart.AI</h1>
       </div>
 
       <nav className="hidden md:flex items-center space-x-8 text-gray-700 font-medium">
